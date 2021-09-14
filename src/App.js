0c71d6c79653fc0f3a22e43045d87c2085ceb877
HRSArgyropoulos <13-13196@saeinstitute.edu>
2021-09-14 14:44:23 +0300

    themeProvider / context

--- src/App.js
+++ src/App.js
@@ -8,29 +8,40 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Rent from './components/Rent';
 import Host from './components/core/Host';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#c585f7',
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <div>
-          <Navbar />
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/to-rent">
-              <Rent />
-            </Route>
-            <Route path="/to-host">
-              <Host />
-            </Route>
-            <Route path="/about">About</Route>
-            <Route path="/contact-us">Contact Us</Route>
-          </Switch>
-        </div>
-      </Router>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Router>
+          <div>
+            <Navbar />
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/to-rent">
+                <Rent />
+              </Route>
+              <Route path="/to-host">
+                <Host />
+              </Route>
+              <Route path="/about">About</Route>
+              <Route path="/contact-us">Contact Us</Route>
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    </ThemeProvider>
   );
 }
 
